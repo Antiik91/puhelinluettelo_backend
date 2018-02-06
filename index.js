bc04bbf2afaff023e741d6bc77fc0c75790efb45
Antiik91 <antiik91@gmail.com>
2018-02-06 19:38:35 +0200

    Initiate app

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
